64ceb668bcb1a7f80572d7a55dcce884b9c9332b
Yoko Izumi <yizumi@nttpc.co.jp>
2017-08-26 05:51:31 +0000

    First commit

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,34 @@
+class ContactsController < ApplicationController
+  def index
+  end
+
+  def create
+    @contact = Contact.create(contacts_params)
+
+    if @contact.save
+      flash[:success] = "お問い合わせが完了しました！"
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  def new
+    if params[:back]
+      @contact = Contact.new(contacts_params)
+    else
+      @contact = Contact.new
+    end
+  end
+
+  def confirm
+    @contact = Contact.new(contacts_params)
+    render :new if @contact.invalid?
+  end
+
+  private
+    def contacts_params
+      params.require(:contact).permit(:name, :email, :content)
+    end
+
+end
