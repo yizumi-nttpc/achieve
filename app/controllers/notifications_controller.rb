329fa8e617efe554eb064f60ba819fe621035936
泉　陽子 <yizumi@nttpc.co.jp>
2017-11-18 14:14:22 +0900

    DIVE19

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,7 +1,7 @@
 class NotificationsController < ApplicationController
-  before_action :authenticate_user!
+#  before_action :authenticate_user!
 
-  def index
-    @notifications = Notification.where(user_id: current_user.id).where(read: false).order(created_at: :desc)
-  end
+#  def index
+#    @notifications = Notification.where(user_id: current_user.id).where(read: false).order(created_at: :desc)
+#  end
 end
