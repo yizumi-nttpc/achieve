50f2798417edc76102dea0026f528e710630c674
泉　陽子 <yizumi@nttpc.co.jp>
2017-11-19 11:24:06 +0900

    migrateファイルを整理

--- db/migrate/20171112150047_add_name_to_messages.rb
+++
@@ -1,5 +0,0 @@
-class AddNameToMessages < ActiveRecord::Migration
-  def change
-    add_column :messages, :read, :boolean, default: false
-  end
-end
--- db/migrate/20171112150823_message_dest.rb
+++
@@ -1,5 +0,0 @@
-class MessageDest < ActiveRecord::Migration
-  def change
-    drop_table :messages
-  end
-end
